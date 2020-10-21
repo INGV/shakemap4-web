68790c530ea87fd6fd0849a5efe7df023e99edd6
Valentino Lauciani <valentino.lauciani@ingv.it>
2020-10-21 17:26:52 +0200

    Update versione

--- softwareVersion.js
+++ softwareVersion.js
@@ -1,1 +1,1 @@
-var softwareVersion = "Website Version: 1.10.0";document.getElementById("footer_text").innerHTML = softwareVersion;
+var softwareVersion = "Website Version: 1.11.0";document.getElementById("footer_text").innerHTML = softwareVersion;
