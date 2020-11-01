bfea451fb5753851e5da3ef597fc6b5c56348a05
Sergio Bruni <sergio.bruni@ingv.it>
2020-11-01 23:45:49 +0100

    responsive functionality to adapt the logo image to the screen width
    responsive functionality to adapt the logo image, the navigation bar and the events table to the screen width. See apropos issue #37

--- elaborate_config.js
+++ elaborate_config.js
@@ -1,4 +1,47 @@
-if (config_data.logo.show) {
-    html_logo = '<img src="' + config_data.logo.image_file + '"/>'
-    document.getElementById("logo").innerHTML = html_logo;
+/*  The name of this file is misleading ...
+    It uses indeed the config_data variable in order to load the logo image
+    but it also contains the functions shared by all pages, since all pages load this file ...
+*/
+
+function loadLogoImage(self){
+    logo_width_percentage = window.screen.width > self.width ? logo_width_percentage =  Math.floor((self.width / window.screen.width) * 100) : 100
+    self.style['width'] = logo_width_percentage + '%';
+    self.style['height'] = 'auto';
 }
+
+
+function include_header(calback) {
+    fetch("./inc/header.html")
+        .then(response => {
+            return response.text()
+        })
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+            calback()
+        });
+}
+
+/* this function is called by header.html. It toggles the menu when it is displayed in mobile mode
+    It is placed here because, as far as I know,  this is the only javascript file
+    loaded by all html pages
+*/
+function toggle_menu_header() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+/*  call the function include_header that include the header.html
+    Only After the function include_header has finished, the logo image can be, in case, loaded
+ */
+include_header(function(){
+    if (config_data.logo) {
+        if (config_data.logo.show) {
+            html_logo = '<img onload="loadLogoImage(this)"  src="' + config_data.logo.image_file + '"/>'
+            document.getElementById("logo").innerHTML = html_logo;
+        }
+    }
+});
