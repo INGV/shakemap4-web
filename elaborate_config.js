0cdb6fb44cf218e6df4eb27f3469bc0420b4dfd7
Sergio Bruni <sergio.bruni@ingv.it>
2020-11-01 23:45:49 +0100

    responsive functionality to adapt the logo image to the screen width

--- elaborate_config.js
+++ elaborate_config.js
@@ -1,4 +1,12 @@
-if (config_data.logo.show) {
-    html_logo = '<img src="' + config_data.logo.image_file + '"/>'
-    document.getElementById("logo").innerHTML = html_logo;
+if (config_data.logo) {
+    if (config_data.logo.show) {
+        html_logo = '<img onload="loadLogoImage(this)"  src="' + config_data.logo.image_file + '"/>'
+        document.getElementById("logo").innerHTML = html_logo;
+    }
+}
+
+function loadLogoImage(self){
+    logo_width_percentage = window.screen.width > self.width ? logo_width_percentage =  Math.floor((self.width / window.screen.width) * 100) : 100
+    self.style['width'] = logo_width_percentage + '%';
+    self.style['height'] = 'auto';
 }
