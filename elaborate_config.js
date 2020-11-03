bd708f89c835e9c88671b69cc747d168c218a9d4
Dario <djozinovi@gmail.com>
2020-11-03 20:19:52 +0100

    Added the image with contributors logo below the table on the home page. Fix #40

--- elaborate_config.js
+++ elaborate_config.js
@@ -2,3 +2,7 @@ if (config_data.logo.show) {
     html_logo = '<img src="' + config_data.logo.image_file + '"/>'
     document.getElementById("logo").innerHTML = html_logo;
 }
+if (config_data.contributorsLogo.show) {
+  html_ContrLogo = '<img src="' + config_data.contributorsLogo.image_file + '"/>'
+  document.getElementById("contributorsLogo").innerHTML = html_ContrLogo;
+}
