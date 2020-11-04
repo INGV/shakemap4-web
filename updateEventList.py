460106e7eee9947e6766bc6048325af914034dcc
Dario <djozinovi@gmail.com>
2020-11-04 09:24:25 +0100

    Fixed a bug with version check if there is styling after the software version value. Issue #39

--- updateEventList.py
+++ updateEventList.py
@@ -136,7 +136,7 @@ def write_version_file():
     with open('softwareVersion.js', 'w') as f:
         print('var softwareVersion = "<span class=\'go_left\'>' + versionElement +
         '";softwareVersion = softwareVersion + ' +
-        '</span>"<span class=\'go_right\'>Development led by INGV ' +
+        '"</span><span class=\'go_right\'>Development led by INGV ' +
         '<a href=\'https://github.com/INGV/shakemap4-web\' target=\'_blank\'>(GitHub) </a></span>";' +
         'document.getElementById("footer_text").innerHTML = softwareVersion;', file=f)
 
