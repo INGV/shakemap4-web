bf11905935fa9dd569652cf61f035190f1c86969
Valentino Lauciani <vlauciani@gmail.com>
2020-11-17 14:30:32 +0100

    Update config-example.js

--- config-example.js
+++ config-example.js
@@ -4,7 +4,11 @@ var config_data = {
         image_file: 'images/ingv_logo_colore.png'
     },
     contributorsLogo: {
-      show: true,
-      image_file: 'images/INGVcontributors.png'
+        show: true,
+        image_file: 'images/INGVcontributors.png'
+    },
+    banner2: {
+	    show: true,
+	    image_file: 'images/banner_lista_terremoti.jpg'
     }
 }
