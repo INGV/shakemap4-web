65f3076977b00f379b0f01c5d072dc1b106fba02
Sergio Bruni <sergio.bruni@ingv.it>
2020-12-15 23:33:43 +0100

    added logo in teh footer and manage footer with a separate footer.html

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,15 +1,4 @@
 
-function include_header(calback) {
-    fetch("./inc/header.html")
-        .then(response => {
-            return response.text()
-        })
-        .then(data => {
-            document.getElementById('header').innerHTML = data;
-            calback()
-        });
-}
-
 /* this function is called by header.html. It toggles the menu when it is displayed in mobile mode
     It is placed here because, as far as I know,  this is the only javascript file
     loaded by all html pages
@@ -27,7 +16,7 @@ function include_disclaimer() {
     if (config_data.disclaimer) {
         if (config_data.disclaimer.show) {
             disclaimer = document.getElementById("disclaimer")
-            if (disclaimer !== null) {
+            if (disclaimer) {
                 fetch(config_data.disclaimer.file)
                     .then(response => {
                         return response.text()
@@ -43,49 +32,77 @@ function include_disclaimer() {
 /*  call the function include_header that include the header.html
     Only After the function include_header has finished, the logo image can be, in case, loaded
  */
-include_header(function(){
-    if (config_data.logo) {
-        if (config_data.logo.show) {
-            logo = document.getElementById("logo")
-            if (logo !== null) {
-                logo.onload = function (self) {
-                    if (window.screen.width > self.target.width) {
+//include_header(function(){
+header_element = document.getElementById('header');
+if (header_element) {
+  fetch("./inc/header.html")
+      .then(response => {
+          return response.text()
+      })
+      .then(data => {
+            header_element.innerHTML = data;
+
+            if (config_data.logo) {
+                if (config_data.logo.show) {
+                    logo = document.getElementById("logo")
+                    if (logo) {
+                        logo.onload = function (self) {
+                            if (window.screen.width > self.target.width) {
+                                if (config_data.contributorsLogo.zoom) {
+                                    self.target.style.width = config_data.logo.zoom
+                                }
+                            }
+                        }
+                        logo.src = config_data.logo.image_file
+                    }
+                }
+            }
+
+            if (config_data.contributorsLogo) {
+                if (config_data.contributorsLogo.show) {
+                    contributorsLogo = document.getElementById("contributorsLogo")
+                    if (contributorsLogo){
+                        contributorsLogo.src = config_data.contributorsLogo.image_file
                         if (config_data.contributorsLogo.zoom) {
-                            self.target.style.width = config_data.logo.zoom
+                            contributorsLogo.style.width = config_data.contributorsLogo.zoom
                         }
                     }
                 }
-                logo.src = config_data.logo.image_file
             }
-        }
-    }
-    if (config_data.contributorsLogo) {
-        if (config_data.contributorsLogo.show) {
-            contributorsLogo = document.getElementById("contributorsLogo")
-            if (contributorsLogo !== null){
-                contributorsLogo.src = config_data.contributorsLogo.image_file
-                if (config_data.contributorsLogo.zoom) {
-                    contributorsLogo.style.width = config_data.contributorsLogo.zoom
+            if (config_data.banner2) {
+                if (config_data.banner2.show) {
+                    banner2_image = document.getElementById("banner2")
+                    if (banner2_image){
+                        banner2_image.src = config_data.banner2.image_file
+                        if (config_data.banner2.zoom) {
+                            banner2_image.style.width = config_data.banner2.zoom
+                        }
+                    }
                 }
             }
 
-            /*
-            html_ContrLogo = '<img src="' + config_data.contributorsLogo.image_file + '"/>'
-            contributorsLogo_container = document.getElementById("contributorsLogo")
-            if (contributorsLogo_container !== null)
-                contributorsLogo_container.innerHTML = html_ContrLogo; */
-        }
-    }
-     if (config_data.banner2) {
-        if (config_data.banner2.show) {
-            banner2_image = document.getElementById("banner2")
-            if (banner2_image !== null){
-                banner2_image.src = config_data.banner2.image_file
-                if (config_data.banner2.zoom) {
-                    banner2_image.style.width = config_data.banner2.zoom
+      });
+}
+
+
+footer_element = document.getElementById('footer');
+if (footer_element) {
+  fetch("./inc/footer.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        footer_element.innerHTML = data;
+        if (config_data.footerLogo) {
+            if (config_data.footerLogo.show) {
+                footerLogo_image = document.getElementById("footerLogo")
+                if (footerLogo_image) {
+                    footerLogo_image.src = config_data.footerLogo.image_file
+                    if (config_data.footerLogo.zoom) {
+                        footerLogo_image.style.width = config_data.footerLogo.zoom
+                    }
                 }
             }
         }
-    }
-
-});
+    });
+}
