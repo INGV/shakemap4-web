6b24833fc181bc3157fc486e8bd00884c59f5670
Valentino Lauciani <valentino.lauciani@ingv.it>
2021-10-22 08:38:12 +0200

    UpdatE

--- Docker/workspace/entrypoint.sh
+++ Docker/workspace/entrypoint.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
+# Set log file
+FILELOG=/tmp/entrypoint.log
+
 # Init pid variable
 pid=0
 
 # Current pid
-echo "Current pid = $$"
+echo "Current pid = $$" | tee -a ${FILELOG}
 
 # Set environment variables
 . ~/.bashrc
 
 # ISGTERM handler
 term_handler () {
-        echo "Run 'service cron stop':"
-        sudo service cron stop
-        echo "Waiting cron stop ..."
+        echo "Run 'service cron stop':" | tee -a ${FILELOG}
+        sudo service cron stop >  | tee -a ${FILELOG} 2>&1 &
+        echo "Waiting cron stop ..." | tee -a ${FILELOG}
         wait "$pid"
-        echo "Done"
+        echo "Done" | tee -a ${FILELOG}
         exit
 }
 
 # With no "exit"
 trap 'term_handler' SIGTERM
 
-echo "Run script to process all events:"
-/usr/bin/python3 /var/www/updateEventList.py > /tmp/entrypoint.log 2>&1 &
-echo "Done"
-echo ""
+echo "Run script to process all events:" | tee -a ${FILELOG}
+/usr/bin/python3 /var/www/updateEventList.py > ${FILELOG} 2>&1 &
+echo "Done" | tee -a ${FILELOG}
+echo "" | tee -a ${FILELOG}
 
-echo "Run 'service cron start':"
-sudo service cron start > /tmp/entrypoint.log 2>&1 &
+echo "Run 'service cron start':" | tee -a ${FILELOG}
+sudo service cron start > ${FILELOG} 2>&1 &
 pid="$!"
-echo "Done"
-echo ""
+echo "Done" | tee -a ${FILELOG}
+echo "" | tee -a ${FILELOG}
 
 while true; do sleep 5; done
