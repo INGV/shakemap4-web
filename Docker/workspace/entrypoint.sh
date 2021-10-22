e5dfb6460d28950bf3b3c5a615d3a1106b3974b5
Valentino Lauciani <valentino.lauciani@ingv.it>
2021-10-22 08:52:56 +0200

    update

--- Docker/workspace/entrypoint.sh
+++ Docker/workspace/entrypoint.sh
@@ -26,14 +26,12 @@ term_handler () {
 trap 'term_handler' SIGTERM
 
 echo "Run script to process all events:" | tee -a ${FILELOG}
-/usr/bin/python3 /var/www/updateEventList.py >> ${FILELOG} 2>&1 &
+/usr/bin/python3 /var/www/updateEventList.py >> ${FILELOG} 2>&1
 echo "Done" | tee -a ${FILELOG}
 echo "" | tee -a ${FILELOG}
 
 echo "Run 'service cron start':" | tee -a ${FILELOG}
 sudo service cron start >> ${FILELOG} 2>&1 &
 pid="$!"
-echo "Done" | tee -a ${FILELOG}
-echo "" | tee -a ${FILELOG}
 
 while true; do sleep 5; done
