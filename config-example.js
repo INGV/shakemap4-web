d58f0c47d369efcedcc32c119db2ad3fca5cf57b
djozinovi <djozinovi@gmail.com>
2022-03-15 11:20:27 +0100

    Replicated the config.js file into config-example.js

---
+++ config-example.js
@@ -0,0 +1,30 @@
+var config_data = {
+    logo: {
+        show: true,
+        image_file: 'images/ingv_logo_colore.png',
+        zoom: '50%'
+    },
+    footerLogo: {
+        show: true,
+        image_file: 'images/footer_INGV.jpg',
+        zoom: '50%'
+    },
+    banner2: {
+        show: true,
+	image_file: 'images/banner_lista_terremoti.jpg',
+        zoom: '100%'
+    },
+    tableNumEventsHome: {
+        numEvents: 6
+    },
+    disclaimer: {
+        show: true,
+        file: 'inc/disclaimer2.html'
+    },
+    vs30gridFile: {
+        file: 'additional_files/Italy_vs30.zip'
+    },
+    dataFolder: {
+        path: 'data/'
+    }
+}
