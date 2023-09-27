02f98260134729a96dcf902910ad99a769f3aefe
Valentino Lauciani <valentino.lauciani@ingv.it>
2023-09-27 09:40:25 +0200

    Fix version number

--- scripts/softwareVersion.js
+++ scripts/softwareVersion.js
@@ -1,4 +1,4 @@
 
-var softwareVersion = "<span class='go_left'>Website Version: v1.27.0";
+var softwareVersion = "<span class='go_left'>Website Version: v1.28.0-dev";
 softwareVersion = softwareVersion + " </span><span class='go_right'>Development of this portal has been made by INGV and it is publicly available at <a href='https://github.com/INGV/shakemap4-web' target='_blank'>GitHub INGV/shakemap4-web  </a></span>";
 document.getElementById("footer_text").innerHTML = softwareVersion;
