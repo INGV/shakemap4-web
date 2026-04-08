ae082f5acb60a5b2329532ae88f9b3a3c40a8a52
Valentino Lauciani <valentino.lauciani@ingv.it>
2026-04-08 13:44:00 +0000

    feat: Update version to "v2.1.0-dev"

--- js/config-base.js
+++ js/config-base.js
@@ -18,7 +18,7 @@ const ShakeMap = {
 };
 
 const config = {
-    version: "v2.0.0",
+    version: "v2.1.0-dev",
     githubLink: "https://github.com/INGV/shakemap4-web",
     disclaimerPage: './disclaimer-ingv.md',
     contributorsPage: './contributors-ingv.md',
