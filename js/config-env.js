04107003e5dc044cfe3d8dbf96d39403d488b6eb
Valentino Lauciani <valentino.lauciani@ingv.it>
2026-04-09 14:45:51 +0000

    chore: Remove unused config-env.js file and restructure profiles

--- js/config-env.js
+++
@@ -1,9 +0,0 @@
-/**
- * ShakeMap Environment Overrides
- *
- * This file is replaced at Docker container startup based on SHAKEMAP_ENV.
- * For local development, it is empty (INGV defaults from config-base.js apply).
- *
- * To test a different environment locally:
- *   cp js/config-eu.js js/config-env.js
- */
